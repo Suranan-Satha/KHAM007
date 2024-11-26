785d18602bb610a8ea59c13d447ec204056a79ca
Suranan-Satha <sherlockharts@gmail.com>
2024-11-26 11:11:59 +0700

    add new project

---
+++ Hello word/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Suranan Satha";
+string student_id = "6730002**-9";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Myname is "+ name);
+Console.WriteLine("My Student id:"+ student_id );
